1bed143fb2cc1354fe4ee588829a22595d8c9793
davidfekke <david@fekke.com>
2019-05-15 22:31:13 -0400

    Adding in weather examples

---
+++ pages/weather.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+class weather extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = { avwx: ''};
+    }
+
+    async componentDidMount() {
+        const results = await axios.get('https://avwx.herokuapp.com/metar/kcrg');
+        const flight_category = results.data.reports[0].flight_category; 
+        this.setState({ avwx: flight_category })
+    }
+
+    render() {
+        return (<div>Current Aviation weather for CRG is {this.state.avwx}.</div>)
+    }
+}
+
+export default weather;
